67b507dff9971ff1d3d6c2b01457166f1362d137
JeffW <40170324+Jeff-Wu97@users.noreply.github.com>
2020-08-09 21:10:51 +0800

    Update tel_redir.py

--- tel_redir.py
+++ tel_redir.py
@@ -6,40 +6,27 @@ Created on Tue Jul 14 09:43:44 2020
 """
 
 import telnetlib
+vm_num=4;
+host_ip=b"localhost"
+vm_port=[5554,5556,5558,5560]
+origin_listen_port=[8000,8002,8004,8006]
+redir_listen_port=[8001,8003,8005,8007]
+emulator_auth=b"auth 1TTxm+dW0V/1oVAV\n"
 
-#def virtual_redir(host_ip,vm_port,listen_port,redir_listen_port):
-tn=telnetlib.Telnet("localhost",5554)
-tn.set_debuglevel(2)
-tn.read_until(b'OK')
-tn.write(b"auth 1TTxm+dW0V/1oVAV\n")
-tn.read_until(b'OK')
-tn.write(b"redir add tcp:8000:8001\n")
-tn.write(b"\n")
-tn.close();
+def virtual_redir(host_ip,vm_port,listen_port,redir_listen_port,emulator_auth):
+    tn=telnetlib.Telnet(host_ip,vm_port)
+    tn.set_debuglevel(2)
+    tn.read_until(b'OK')
+    tn.write(emulator_auth)
+    tn.read_until(b'OK')
+    add_command="redir add tcp:"+str(listen_port)+":"+str(redir_listen_port)+"\n"
+    tn.write(add_command.encode('utf-8'))
+    tn.write(b"\n")
+    tn.close();
+    
 
-tn=telnetlib.Telnet("localhost",5556)
-tn.set_debuglevel(2)
-tn.read_until(b'OK')
-tn.write(b"auth 1TTxm+dW0V/1oVAV\n")
-tn.read_until(b'OK')
-tn.write(b"redir add tcp:8002:8003\n")
-tn.write(b"\n")
-tn.close();
-
-tn=telnetlib.Telnet("localhost",5558)
-tn.set_debuglevel(2)
-tn.read_until(b'OK')
-tn.write(b"auth 1TTxm+dW0V/1oVAV\n")
-tn.read_until(b'OK')
-tn.write(b"redir add tcp:8004:8005\n")
-tn.write(b"\n")
-tn.close();
-
-tn=telnetlib.Telnet("localhost",5560)
-tn.set_debuglevel(2)
-tn.read_until(b'OK')
-tn.write(b"auth 1TTxm+dW0V/1oVAV\n")
-tn.read_until(b'OK')
-tn.write(b"redir add tcp:8006:8007\n")
-tn.write(b"\n")
-tn.close();
+for i in range(0,len(vm_port)):
+    try:
+        virtual_redir(host_ip, vm_port[i], origin_listen_port[i], redir_listen_port[i], emulator_auth)
+    except ConnectionRefusedError:
+        print("ERROR: target compter denied : "+host_ip+" "+str(vm_port[i]))
